5bea540f1fbd07e4bcd9d53a24d214a0e06e5786
WJ Boshoff <wjbosdev@gmail.com>
2025-10-31 20:54:03 +0200

    moved listener

--- src/main/java/wbos/starterchatapp/config/listeners/WebsocketEventListener.java
+++ src/main/java/wbos/starterchatapp/config/listeners/WebsocketEventListener.java
@@ -3,17 +3,34 @@ package wbos.starterchatapp.config.listeners;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import wbos.starterchatapp.dtos.ChatMessage;
+import wbos.starterchatapp.enums.MessageType;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class WebsocketEventListener {
 
+    private final SimpMessageSendingOperations messageSender;
+
     @EventListener
     public void handleWebsocketDisconnectListener(SessionDisconnectEvent disconnectEvent){
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(disconnectEvent.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
 
+        if(username != null){
+            log.info("User disconnected: {}", username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+
+            messageSender.convertAndSend("/topic/public",chatMessage);
+        }
     }
 
 }
