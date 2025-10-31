0f1e1ac7b85805de3f742778dff0e8cf94ce85c0
WJ Boshoff <wjbosdev@gmail.com>
2025-10-31 21:25:28 +0200

    Created MiddleWare Module

---
+++ middleWare/src/main/java/wbos/middleware/MiddleWareApplication.java
@@ -0,0 +1,13 @@
+package wbos.middleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddleWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiddleWareApplication.class, args);
+    }
+
+}
